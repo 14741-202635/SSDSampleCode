11ab9d01c3138df583934052d9f55116c0a956bf
Mohamed Elliethy <mohamed.elliethy@mohawkcollege.ca>
2026-09-16 00:25:45 -0400

    Add project files.

---
+++ L01SampleAuth/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace L01SampleAuth.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+    }
+
+    //Data Tables
+
+}
---
+++ L01SampleAuth/Data/ApplicationUser.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+namespace L01SampleAuth.Data;
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; }
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
+
+}
